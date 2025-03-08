f09995be8348fe036b2640e8b3a5ae522b9b7e9d
Jayson <jaysonwelshman@gmail.com>
2025-03-07 20:00:10 -0500

    I have added a command group that runs autnomously.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,8 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
@@ -45,6 +47,13 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
+    new SequentialCommandGroup(
+      new ElevatorPIDcmd(elevatorsubsystem, Elevatorconstants.kElevatorSetpointgoto), //
+
+      new ParallelCommandGroup(
+        new Intakecmd(intakesubsystem, Intakeconstants.kIntakeSpeedin) //
+      )//
+    );
     return null;
   }
 }
